0c3a9ab90a5a2273c411801330f0da471b3d8f1d
Aedyn Ladd <55495779+AedynLadd@users.noreply.github.com>
2022-02-25 14:00:46 -0500

    another way to add a script - v1 stuff

---
+++ robot_model/scripts/TestLuaFile_contained.lua
@@ -0,0 +1,13 @@
+
+function sysCall_init()
+    -- do some initialization here
+    print("hello world")
+end
+
+function sysCall_actuation()
+    -- put your actuation code here
+end
+
+function sysCall_sensing()
+    -- put your sensing code here
+end
