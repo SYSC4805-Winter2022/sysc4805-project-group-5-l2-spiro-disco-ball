7682e1c3297c8486c673068193b34dbe8a2abd10
David Casciano <50248611+DavidCasc@users.noreply.github.com>
2022-02-24 22:42:48 -0500

    Added External Script

--- TestLuaFile.lua
+++ TestLuaFile.lua
@@ -1,10 +1,13 @@
 function init_body()
     -- do some initialization here
     print("hello world")
+    leftJoint=sim.getObjectHandle("left_wheel_motor") 
+    nominalLinearVelocity=1
 end
 
 function actuation_body()
     -- put your actuation code here
+    sim.setJointTargetVelocity(leftJoint,1)
 end
 
 function sensing_body()
@@ -14,3 +17,4 @@ end
 function cleanup_body()
     -- do some clean-up here
 end
+
